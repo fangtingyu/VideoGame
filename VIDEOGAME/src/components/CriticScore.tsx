cfcc21b0a7ae23c2ec3d2ddabec64a5b24895372
yu_fangting <fangting.yu@metrocount.net>
2023-04-18 10:34:27 +0800

    Displaying critic score

---
+++ VIDEOGAME/src/components/CriticScore.tsx
@@ -0,0 +1,19 @@
+import { Badge } from '@chakra-ui/react';
+import React from 'react'
+
+// This component is used for rendering the critic score
+
+interface Props {
+    score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+    let color = score > 75 ? 'green' : score > 65 ? 'yellow' : '';
+    return (
+        <Badge colorScheme={color} fontSize='14px' paddingX={2} borderRadius='4px'>
+            {score}
+        </Badge>
+    )
+}
+
+export default CriticScore
