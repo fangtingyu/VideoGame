30e9376c2106051f5ed3dc017d1b054630277f55
yu_fangting <fangting.yu@metrocount.net>
2023-04-18 12:18:19 +0800

    fixed export issue in APP.tsx

--- VIDEOGAME/src/App.tsx
+++ VIDEOGAME/src/App.tsx
@@ -1,40 +1,30 @@
 import { useState } from 'react'
-import GameFilter from './components/GameFilter'
 import GameGrid from './components/GameGrid'
-import GenresFilter from './components/GenresFilter'
-import ThemeChange from './components/ThemeChange'
 import GameSearchBar from './components/GameSearchBar'
 import "bootstrap/dist/css/bootstrap.css"
 import { Grid, GridItem, Show } from '@chakra-ui/react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const [count, setCount] = useState(0)
 
-  return (
-    <Grid templateAreas={{
-      base: ` "nav" "main" `,
-      lg: ` "nav nav" "aside main" ` // large devices are devices that larger than 1240px
-    }}>
-      <GridItem area='nav'>
-        <GameSearchBar />
-      </GridItem>
-      <Show above="lg">
-        <GridItem area='aside' bg='gold' >
-          Aside
-        </GridItem>
-      </Show>
-      <GridItem area='main' >
-        <GameGrid />
-      </GridItem>
-    </Grid>
-    // <div className="App">
-    //   <GameSearchBar /> 
-    //   <GameList />
-    //   <GameFilter />
-    //   <GenresFilter onSelectGenre={(genre) => console.log(genre)} />
-
-    // </div>
-  )
+    return (
+        <Grid templateAreas={{
+            base: ` "nav" "main" `,
+            lg: ` "nav nav" "aside main" ` // large devices are devices that larger than 1240px
+        }}>
+            <GridItem area='nav'>
+                <GameSearchBar />
+            </GridItem>
+            <Show above="lg">
+                <GridItem area='aside' bg='gold' >
+                    Aside
+                </GridItem>
+            </Show>
+            <GridItem area='main' >
+                <GameGrid />
+            </GridItem>
+        </Grid>
+    )
 }
 
 export default App
