750e5550c12fc11db6c510904074f41f721382ba
yu_fangting <fangting.yu@metrocount.net>
2023-04-27 10:22:58 +0800

    Sort the Games

--- VIDEOGAME/src/hooks/useGames.ts
+++ VIDEOGAME/src/hooks/useGames.ts
@@ -23,7 +23,8 @@ const useGames = (
         {
             params: {
                 genres: gameQuery.genre?.id,
-                platform: gameQuery.platform?.id
+                platform: gameQuery.platform?.id,
+                ordering: gameQuery.sortOrder
             }
         },
         [gameQuery]
