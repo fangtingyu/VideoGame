c3d8cfbe6531ee4bcfce27edb11b71ab83512e0f
yu_fangting <fangting.yu@metrocount.net>
2023-06-21 13:33:51 +0800

    Exercise - Working with Reducers

---
+++ react-course-part2/src/state-management/reducers/authReducer.ts
@@ -0,0 +1,22 @@
+interface Action {
+    type: 'LOGIN' | 'LOGOUT';
+}
+
+interface LoginAction {
+    type: "LOGIN"
+    username: string
+}
+
+interface LogoutAction {
+    type: "LOGOUT"
+}
+
+type AuthAction = LoginAction | LogoutAction
+
+const authReducer = (state: string, action: AuthAction): string => {
+    if (action.type === 'LOGIN') return action.username;
+    if (action.type === 'LOGOUT') return '';
+    return state;
+}
+
+export default authReducer;
